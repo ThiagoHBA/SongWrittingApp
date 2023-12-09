40c91c19b994db10345fd354fbc7338f62dd96b6
ThiagoHBA <thiagohhab@gmail.com>
2023-12-09 15:20:46 -0300

    creating UI module + adding dependencies

---
+++ UI/DiscoListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DiscoListViewController.swift
+//  UI
+//
+//  Created by Thiago Henrique on 09/12/23.
+//
+
+import UIKit
+
+class DiscoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
