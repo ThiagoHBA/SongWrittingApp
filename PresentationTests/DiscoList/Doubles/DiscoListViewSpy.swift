b81713088d10b3d268ca0ebd96ce40d203962e19
ThiagoHBA <thiagohhab@gmail.com>
2023-12-10 17:13:35 -0300

    structuring DiscoListViewSpy

--- PresentationTests/DiscoList/Doubles/DiscoListViewSpy.swift
+++ PresentationTests/DiscoList/Doubles/DiscoListViewSpy.swift
@@ -8,34 +8,65 @@
 import Foundation
 import Presentation
 
-final class DiscoListViewSpy {}
+final class DiscoListViewSpy {
+    private(set) var receivedMessages: [Message] = [Message]()
+    
+    enum Message: Equatable, CustomStringConvertible {
+        case startLoading
+        case hideLoading
+        case hideOverlays
+        case showDiscos([DiscoListViewEntity])
+        case showNewDisco(DiscoListViewEntity)
+        case createDiscoError(String, String)
+        case loadDiscoError(String, String)
+        
+        var description: String {
+            switch self {
+                case .startLoading:
+                    return "startLoading Called"
+                case .hideLoading:
+                    return "hideLoading Called"
+                case .hideOverlays:
+                    return "hideOverlays Called"
+                case .showDiscos(let discos):
+                    return "showDiscos Called with data: \(discos)"
+                case .showNewDisco(let newDisco):
+                    return "showNewDisco Called with data: \(newDisco)"
+                case .createDiscoError(let title, let message):
+                    return "createDiscoError Called with data: \(title), \(message)"
+                case .loadDiscoError(let title, let message):
+                    return "loadDiscoError Called with data: \(title), \(message)"
+            }
+        }
+    }
+}
 
 extension DiscoListViewSpy: DiscoListDisplayLogic {
     func startLoading() {
-            
+        receivedMessages.append(.startLoading)
     }
     
     func hideLoading() {
-        
+        receivedMessages.append(.hideLoading)
     }
     
     func hideOverlays(completion: (() -> Void)?) {
-        
+        receivedMessages.append(.hideLoading)
     }
     
-    func showDiscos(_ discos: [Presentation.DiscoListViewEntity]) {
-        
+    func showDiscos(_ discos: [DiscoListViewEntity]) {
+        receivedMessages.append(.showDiscos(discos))
     }
     
-    func showNewDisco(_ disco: Presentation.DiscoListViewEntity) {
-        
+    func showNewDisco(_ disco: DiscoListViewEntity) {
+        receivedMessages.append(.showNewDisco(disco))
     }
     
     func createDiscoError(_ title: String, _ description: String) {
-        
+        receivedMessages.append(.createDiscoError(title, description))
     }
     
     func loadDiscoError(_ title: String, _ description: String) {
-        
+        receivedMessages.append(.loadDiscoError(title, description))
     }
 }
