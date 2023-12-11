5288d1f1555d32d8ed89e1d83360f607d08a1162
ThiagoHBA <thiagohhab@gmail.com>
2023-12-11 12:12:21 -0300

    finishing use case

--- Domain/UseCase/AddDiscoNewReference/AddDiscoNewReferenceUseCase.swift
+++ Domain/UseCase/AddDiscoNewReference/AddDiscoNewReferenceUseCase.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public final class AddDiscoNewReferenceUseCase {
     let service: DiscoService
-    public var output: AddDiscoNewReferenceUseCaseOutput?
+    public var output: [AddDiscoNewReferenceUseCaseOutput]?
     public var input: AddDiscoNewReferenceInput?
     
     init(service: DiscoService) {
@@ -20,6 +20,26 @@ public final class AddDiscoNewReferenceUseCase {
         assert(input != nil)
         guard let input = input else { return }
         
-        
+        service.updateDiscoReferences(
+            input.disco,
+            references: input.newReferences
+        ) { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+                case .success(let profile):
+                    output?.forEach {
+                        $0.successfullyAddNewReferences(
+                            to: profile.disco,
+                            references: profile.references
+                        )
+                    }
+                case .failure(let error):
+                    output?.forEach {
+                        $0.errorWhileAddingNewReferences(error)
+                    }
+            }
+            
+        }
     }
 }
