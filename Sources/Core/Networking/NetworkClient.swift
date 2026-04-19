9c03fa621662f60dfa31950102cbd42c41b4432e
ThiagoHBA <thiagohhab@gmail.com>
2026-04-19 16:26:30 -0300

    adjusting missing declarations on the NetworkClient

--- Sources/Core/Networking/NetworkClient.swift
+++ Sources/Core/Networking/NetworkClient.swift
@@ -7,6 +7,64 @@
 
 import Foundation
 
+public protocol Endpoint {
+    var communicationProtocol: CommunicationProtocol { get }
+    var urlBase: String { get }
+    var path: String { get }
+    var httpMethod: HTTPMethod? { get }
+    var body: Data? { get set }
+    var headers: [String: String] { get set }
+    var queries: [URLQueryItem] { get }
+}
+
+public enum CommunicationProtocol: String {
+    case HTTP = "http"
+    case HTTPS = "https"
+}
+
+public enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case delete = "DELETE"
+}
+
+public extension Endpoint {
+    var httpMethod: HTTPMethod { return .get }
+
+    var headers: [String: String] {
+        let defaultHeaders = [
+            "Content-Type": "application/json"
+        ]
+        return defaultHeaders
+    }
+
+    var queries: [URLQueryItem] { return [] }
+
+    var body: Data? { return nil }
+
+    func makeURL() -> URL? {
+        guard var component = URLComponents(
+            string: "\(communicationProtocol.rawValue)://\(urlBase)\(path)"
+        ) else {
+            return nil
+        }
+        component.scheme = communicationProtocol.rawValue
+        component.queryItems = queries.isEmpty ? nil : queries
+        return component.url
+    }
+}
+
+
+public protocol URLSessionProtocol {
+    func dataTask(
+        with request: URLRequest,
+        completionHandler: @escaping @Sendable (Data?, URLResponse?, Error?) -> Void
+    ) -> URLSessionDataTask
+}
+
+extension URLSession: URLSessionProtocol {}
+
+
 public protocol NetworkClient {
     func makeRequest(
         _ endpoint: Endpoint,
